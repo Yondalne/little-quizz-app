47b5d71fe39c0ccfcf534c3fba5e5750288ef5a9
Yondalne <benalicherif.yd@gmail.com>
2025-11-27 00:27:28 +0000

    first version

---
+++ src/components/Result.vue
@@ -0,0 +1,38 @@
+<template>
+    <div class="result">
+        <div class="title">{{ results[resultIndex].title }}</div>
+        <div class="desc">
+            {{ results[resultIndex].desc }}
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "Result",
+    props: {
+        results: {
+            type: Object,
+            required: true
+        },
+        totalCorrect: {
+            type: Number,
+            required: true
+        }
+    },
+    computed: {
+        resultIndex() {
+            let index = 0;
+            this.results.forEach((result, i) => {
+                if (this.totalCorrect >= result.min && this.totalCorrect <= result.max) {
+                    index = i;
+                }
+            });
+
+            return index;
+        }
+    }
+};
+</script>
+
+<style></style>
